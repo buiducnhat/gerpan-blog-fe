836b01ffaa6c21e32ecf6684a56723b07e5bda14
gerpan <nhaths4701@gmail.com>
2021-10-05 09:22:31 +0700

    Edit tag

--- src/components/article-tags/article-tags-random.tsx
+++ src/components/article-tags/article-tags-random.tsx
@@ -1,6 +1,8 @@
-import { Flex, Tag } from '@chakra-ui/react';
+import Link from 'next/link';
+import { Flex, Tag, Icon, HStack, Text } from '@chakra-ui/react';
 import { ThemeTypings } from '@chakra-ui/styled-system';
 import { IArticleTagBasic } from '@src/models/article-tag.interface';
+import { FaTags as TagsIcon } from 'react-icons/fa';
 
 export interface IArticleTagsRandomProps {
   tags: IArticleTagBasic[];
@@ -31,11 +33,27 @@ export const ArticleTagsRandom = ({ tags }: IArticleTagsRandomProps) => {
   };
 
   return (
-    <Flex wrap="wrap" justify="center">
+    <Flex wrap="wrap" justify="center" align="center">
+      <HStack mr="3">
+        <Text as="h3" fontWeight="semibold" color="GrayText">
+          {'Tags'}
+        </Text>
+        <Icon as={TagsIcon} w="5" h="5" color="primary.500" />
+      </HStack>
+
       {tags.map((tag) => (
-        <Tag key={tag.id} p='2' m='0.5' variant="solid" colorScheme={getTagColor(tag.id)}>
-          {tag.title}
-        </Tag>
+        <Link key={tag.id} href="#" passHref>
+          <Tag
+            p="2"
+            m="0.5"
+            rounded="xl"
+            variant="solid"
+            colorScheme={getTagColor(tag.id)}
+            cursor="pointer"
+          >
+            {tag.title}
+          </Tag>
+        </Link>
       ))}
     </Flex>
   );
