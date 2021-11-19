3abc2f8ce9a9e2e93c5c8ae2ffec00ba43f264e9
gerpan <nhaths4701@gmail.com>
2021-11-19 14:36:35 +0700

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const securityHeaders = [
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin'
-  }
-];
-
 module.exports = {
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/(.*)',
-        headers: securityHeaders
-      }
-    ];
-  }
+  reactStrictMode: true
 };
