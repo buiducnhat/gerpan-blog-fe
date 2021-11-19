f70872cf85aeb9a6013ecc03b7a928d3c14a0e1e
gerpan <nhaths4701@gmail.com>
2021-11-19 14:23:08 +0700

    Config next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
+];
+
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders
+      }
+    ];
+  }
 };
